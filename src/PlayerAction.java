dcb984c0bec1fe530f466b6d9eea2054b78fa313
Anton Tepe <antonjakobtepe@gmail.com>
2026-04-03 22:12:51 +0200

    Player Action Attribute + Konstruktor

--- src/PlayerAction.java
+++ src/PlayerAction.java
@@ -1,2 +1,11 @@
 public class PlayerAction {
+    private int player;
+    private int xpos;
+    private int ypos;
+
+    public PlayerAction(String input, int player) {
+        this.xpos = Character.getNumericValue(input.charAt(0));
+        this.ypos = Character.getNumericValue(input.charAt(2));
+        this.player = player;
+    }
 }
